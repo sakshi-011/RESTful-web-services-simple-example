d843c0568d8740a0492682bee775b01873d9a180
sakshi-011 <sakshi.singhania011@gmail.com>
2022-05-23 17:40:48 +0530

    Added documentation using Swagger

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,16 +1,22 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Details about User.")
 public class User {
 
     private Integer id;
 
-    @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should consist of at least 2 characters.")
+    @Size(min = 2, message = "Name should have at least 2 characters.")
     private String name;
 
+    @ApiModelProperty(notes = "Birth date should be less than current date.")
     @Past
     private Date birthDate;
 
